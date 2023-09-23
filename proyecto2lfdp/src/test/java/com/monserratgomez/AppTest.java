12a58c19b2f5cb4824876f4a21489953a4a36163
Monserrat Gomez <mariagomez202030849@cunoc.edu.gt>
2023-09-22 23:50:18 -0600

    Agregados archivos iniciales

---
+++ proyecto2lfdp/src/test/java/com/monserratgomez/AppTest.java
@@ -0,0 +1,20 @@
+package com.monserratgomez;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
